d2263eda0647dfec5c8d17751f4c361bd1f7b361
JaneyRG <jan.giernatowski@googlemail.com>
2026-04-24 23:54:48 +0100

    chat gbt modded code so I can use SQLite3

--- Database/dblib/dblib/framework.h
+++ Database/dblib/dblib/framework.h
@@ -1,5 +1,15 @@
+// This header centralises the inclusion of platform‑specific system headers.  On
+// Windows the WIN32_LEAN_AND_MEAN macro reduces the amount of code pulled in
+// from the Windows headers.  On non‑Windows platforms the windows.h header
+// does not exist, so we avoid including it by wrapping in an appropriate
+// preprocessor check.
+
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN
+
 // Windows Header Files
-#include <windows.h>
+#ifdef _WIN32
+#  include <windows.h>
+#endif
--- SQLlite3/SqlDbHandler.cpp
+++
@@ -1,6 +0,0 @@
-#include "pch.h"
-#include "SqlDbHandler.h"
-
-SqlDbHandler::~SqlDbHandler()
-{
-}
